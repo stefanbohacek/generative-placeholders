dfd681717b6e98fc6f4af965ce7f3832542946ac
Glitch (generative-placeholders) <none>
2020-07-24 12:48:51 +0000

    Updated descriptions.

---
+++ routes/stats.js
@@ -0,0 +1,28 @@
+const express = require( 'express' ),
+  fs = require( 'fs' ),
+  palettes = require('nice-color-palettes'),      
+  router = express.Router();
+
+router.get( '/', function( req, res ) {
+  const filePath = `${__dirname}/../.data/stats.json`;
+  
+  fs.readFile( filePath, 'utf8', function ( err, data ) {
+    
+    try{
+      data = JSON.parse( data );
+    } catch ( err ){
+      data = {};
+    }
+    
+    res.render( '../views/stats.handlebars', {
+      data: data,
+      project_name: process.env.PROJECT_DOMAIN,
+      sc_project: process.env.SC_PROJECT,
+      sc_security: process.env.SC_SECURITY,
+      palettes: palettes,    
+      timestamp: Date.now()
+    } );
+  } );
+} );
+
+module.exports = router;
