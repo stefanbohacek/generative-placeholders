11751d75fede6f77ff1b802cf2485e0ce00d38d9
Glitch (generative-placeholders) <none>
2023-03-04 22:20:06 +0000

    Updated author link.

--- data-sync.js
+++ data-sync.js
@@ -1,47 +1,47 @@
-const fs = require( 'fs' ),
-      helpers = require( __dirname + '/helpers/general.js' ),      
-      Twit = require( 'twit' ),    
+const fs = require('fs'),
+      helpers = require(__dirname + '/helpers/general.js'),      
+      Twit = require('twit'),    
       dataFilePath = __dirname + '/data/data.json',
       botListFilePath = __dirname + '/data/bots.json';
 
 let dataSync = {
-  reload: function( cb ){
-    console.log( 'reloading data...' );
+  reload: function(cb){
+    console.log('reloading data...');
     
-    let twit = new Twit( {
+    let twit = new Twit({
       consumer_key: process.env.TWITTER_CONSUMER_KEY,
       consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
       app_only_auth: true
-    } );
+    });
     
-    twit.get( 'lists/members', {
+    twit.get('lists/members', {
       owner_screen_name: process.env.TWITTER_USERNAME,
       slug: process.env.TWITTER_LIST_NAME,
       count: 5000,
       skip_status: true
-    }, function( err, data, response ) {
-      if ( err ){
-        console.log( 'error:', err );
+    }, function(err, data, response) {
+      if (err){
+        console.log('error:', err);
       }
-      else if ( data && data.users ){
+      else if (data && data.users){
         let bots = [];
-        data.users.forEach(  function ( user ){
-          bots.push( user.screen_name );
-        } );
+        data.users.forEach( function (user){
+          bots.push(user.screen_name);
+        });
 
-        console.log( bots );
+        console.log(bots);
     
-        if ( bots && bots.length ){
+        if (bots && bots.length){
 
-          twit.get( 'users/lookup', {
+          twit.get('users/lookup', {
             screen_name: bots.join(',')
-          }, function( err, data, response ) {
-            if ( err ){
-              console.log( err );
+          }, function(err, data, response) {
+            if (err){
+              console.log(err);
             } else {
               let updatedBotData = [];
-              data.forEach( function( bot ){
-                  updatedBotData.push( {
+              data.forEach(function(bot){
+                  updatedBotData.push({
                       id_str: bot.id_str,
                       name: bot.name,
                       screen_name: bot.screen_name,
@@ -55,69 +55,69 @@ let dataSync = {
                       statuses_count: bot.statuses_count,
                       profile_background_image_url_https: bot.profile_background_image_url_https,
                       profile_image_url_https: bot.profile_image_url_https
-                  } );
-              } );
+                  });
+              });
 
-              // console.log( updatedBotData );
+              // console.log(updatedBotData);
 
-              fs.writeFile( dataFilePath, JSON.stringify( {
+              fs.writeFile(dataFilePath, JSON.stringify({
                 last_update: Date.now(),
-                data: updatedBotData.sort( helpers.sortByFollowersCount )
-              } ), function ( err ) {
-               if ( err ){
-                 console.log( err )
+                data: updatedBotData.sort(helpers.sortByFollowersCount)
+              }), function (err) {
+               if (err){
+                 console.log(err)
                } else {
-                  console.log( 'data file updated' );
+                  console.log('data file updated');
                }
-              } );  
-              if ( cb ){
-                cb( null, updatedBotData );
+              });  
+              if (cb){
+                cb(null, updatedBotData);
               }
             }
-          } );        
+          });        
         }         
       }
-    } ); 
+    }); 
   },
-  sync: function( cb ){
-    let botData = fs.readFileSync( dataFilePath ),
+  sync: function(cb){
+    let botData = fs.readFileSync(dataFilePath),
         dataSync = this;
 
     try{
-      botData = JSON.parse( botData );
-    } catch ( err ){ console.log( err ) }
+      botData = JSON.parse(botData);
+    } catch (err){ console.log(err) }
 
-    if ( botData ){
-      // console.log( botData );
-      botData.data = botData.data.sort( helpers.sortByFollowersCount );
+    if (botData){
+      // console.log(botData);
+      botData.data = botData.data.sort(helpers.sortByFollowersCount);
       
       let syncIntervalMinutes = 60;
 
-      if ( process.env.SYNC_INTERVAL_MINUTES && process.env.SYNC_INTERVAL_MINUTES > 15 ){
-        syncIntervalMinutes = parseInt( process.env.SYNC_INTERVAL_MINUTES );
+      if (process.env.SYNC_INTERVAL_MINUTES && process.env.SYNC_INTERVAL_MINUTES > 15){
+        syncIntervalMinutes = parseInt(process.env.SYNC_INTERVAL_MINUTES);
       }
       
       var todayDate = new Date(),
-          lastCheckDate = new Date( botData.last_update ),
-          expirationDate = new Date( lastCheckDate.getTime() + ( syncIntervalMinutes * 60000 ) );
+          lastCheckDate = new Date(botData.last_update),
+          expirationDate = new Date(lastCheckDate.getTime() + (syncIntervalMinutes * 60000));
       
-      console.log( {
+      console.log({
           'lastCheckDate': lastCheckDate,
-          'syncIntervalMinutes' : `${syncIntervalMinutes} (${ ( syncIntervalMinutes/60 ).toFixed( 3 ) } hours)`,
+          'syncIntervalMinutes' : `${syncIntervalMinutes} (${ (syncIntervalMinutes/60).toFixed(3) } hours)`,
           'expirationDate': expirationDate,
           'todayDate': todayDate,
           'dataExpired': todayDate > expirationDate,
           // 'botData': botData
-      } );
+      });
       
-      if ( !botData || !botData.data || !botData.data.length || todayDate > expirationDate ){
+      if (!botData || !botData.data || !botData.data.length || todayDate > expirationDate){
         /* Update data */
-        dataSync.reload( function( err, data ){
-          cb( null, data );
+        dataSync.reload(function(err, data){
+          cb(null, data);
         });
       } else {
-        if ( cb ){
-          cb( null, botData );
+        if (cb){
+          cb(null, botData);
         }
       }
     }
